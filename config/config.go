e6d1852386a70054ec4e5126d56964fb1718afa7
ZSQ <zhanmabigdata@outlook.com>
2023-01-12 23:28:32 +0800

    add json feature to config

--- config/config.go
+++ config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
 type Config struct {
 	Segment struct {
 		Store struct {
@@ -9,4 +14,21 @@ type Config struct {
 			MaxBytes uint64
 		}
 	}
+}
+
+func NewConfig(path string) (*Config, error) {
+
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Config{}
+	err = json.Unmarshal(content, c)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+
 }
