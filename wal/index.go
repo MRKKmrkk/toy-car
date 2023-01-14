44cdc7e4de749246e1bc573746839115bbcb421a
ZSQ <zhanmabigdata@outlook.com>
2023-01-14 20:35:02 +0800

    make sure thread safe on the wal

--- wal/index.go
+++ wal/index.go
@@ -1,21 +1,18 @@
 package wal
 
 import (
-	"bufio"
-	"encoding/binary"
 	"io"
 	"os"
-	"sync"
 	"toy-car/config"
+
+	"github.com/tysonmote/gommap"
 )
 
 // struct index maintain a file which use to store index of record
 type index struct {
-	file     *os.File
-	size     uint64
-	capacity uint64
-	mu       sync.Mutex
-	buf      *bufio.Writer
+	file *os.File
+	size uint64
+	mmap gommap.MMap
 }
 
 const (
@@ -32,35 +29,45 @@ func NewIndex(f *os.File, config *config.Config) (*index, error) {
 		return nil, err
 	}
 
+	// apply memory as config.Segment.Store.MaxBytes to map file
+	size := info.Size()
+	err = os.Truncate(f.Name(), int64(config.Segment.Index.MaxBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	mmap, err := gommap.Map(
+		f.Fd(),
+		gommap.PROT_READ|gommap.PROT_WRITE,
+		gommap.MAP_SHARED,
+	)
+
 	return &index{
-		size:     uint64(info.Size()),
-		capacity: config.Segment.Index.MaxBytes,
-		file:     f,
-		buf:      bufio.NewWriter(f),
+		size: uint64(size),
+		mmap: mmap,
+		file: f,
 	}, nil
 
 }
 
 func (i *index) Write(offset uint32, position uint64) error {
 
-	i.mu.Lock()
-	defer i.mu.Unlock()
+	i.mmap.Lock()
+	defer i.mmap.Unlock()
 
 	// return error if mmap is full
-	if i.capacity < i.size+OFFSET_AND_POSITION_WIDTH {
+	if len(i.mmap) < int(i.size)+OFFSET_AND_POSITION_WIDTH {
 		return io.EOF
 	}
 
-	err := binary.Write(i.buf, enc, offset)
-	if err != nil {
-		return err
-	}
-	err = binary.Write(i.buf, enc, position)
-	if err != nil {
-		return err
-	}
+	enc.PutUint32(i.mmap[i.size:i.size+OFFSET_WIDTH], offset)
+	enc.PutUint64(i.mmap[i.size+OFFSET_WIDTH:i.size+OFFSET_AND_POSITION_WIDTH], position)
 
 	i.size += OFFSET_AND_POSITION_WIDTH
+	err := os.Truncate(i.file.Name(), int64(i.size))
+	if err != nil {
+		return err
+	}
 
 	return nil
 
@@ -71,13 +78,8 @@ func (i *index) Write(offset uint32, position uint64) error {
 // return (offset uint32, pos uint64, err error)
 func (i *index) Read(offset int64) (uint32, uint64, error) {
 
-	i.mu.Lock()
-	defer i.mu.Unlock()
-
-	err := i.buf.Flush()
-	if err != nil {
-		return 0, 0, err
-	}
+	i.mmap.Lock()
+	defer i.mmap.Unlock()
 
 	if i.size == 0 {
 		return 0, 0, nil
@@ -93,26 +95,27 @@ func (i *index) Read(offset int64) (uint32, uint64, error) {
 
 	// figure out actually position
 	cur := off * OFFSET_AND_POSITION_WIDTH
-	if cur > uint32(i.capacity) {
+	if cur >= uint32(i.size) {
 		return 0, 0, io.EOF
 	}
 
-	buffer := make([]byte, OFFSET_AND_POSITION_WIDTH)
-	_, err = i.file.ReadAt(buffer, int64(cur))
-	if err != nil {
-		return 0, 0, err
-	}
-
-	return enc.Uint32(buffer[:OFFSET_WIDTH]), enc.Uint64(buffer[OFFSET_WIDTH:]), nil
+	off = enc.Uint32(i.mmap[cur : cur+OFFSET_WIDTH])
+	return off, enc.Uint64(i.mmap[cur+OFFSET_WIDTH : cur+OFFSET_AND_POSITION_WIDTH]), nil
 
 }
 
 func (i *index) Close() error {
 
-	i.mu.Lock()
-	defer i.mu.Unlock()
+	i.mmap.Lock()
+	defer i.mmap.Unlock()
 
-	err := i.buf.Flush()
+	err := i.mmap.Sync(gommap.MS_SYNC)
+	if err != nil {
+		return err
+	}
+
+	// change index file to real size
+	err = os.Truncate(i.file.Name(), int64(i.size))
 	if err != nil {
 		return err
 	}
